37e3f209342e4f003908fb0f51757c3ed0ac434b
Omar Rmz <omar.cruzr97@gmail.com>
2024-02-27 13:20:39 -0600

    actualizando

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,32 @@ import React, { useState, useEffect } from "react";
 import sanityClient from '../client'
 import '../styles/banner.css'
 
-export default function Banner(){
-    return(
+export default function Banner() {
+    const [banners, setBanners] = useState([]);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "banner"]{
+            title,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            }
+        }`)
+        .then(data => setBanners(data))
+        .catch(error => console.error("Error fetching banners:", error));
+    }, []);
+
+    return (
         <main className="banner">
-            banner
+            {banners.map((banner, index) => (
+                <div key={index}>
+                    <h2>{banner.title}</h2>
+                    <img src={banner.mainImage.asset.url} alt={banner.mainImage.alt} />
+                </div>
+            ))}
         </main>
-    )
+    );
 }
